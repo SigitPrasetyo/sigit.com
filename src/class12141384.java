14aa0983683d76491a8edc96ffb82f4e4eb31e98
Mr.She-Gate <Mr.She-Gate@She-Gate>
2016-11-22 13:10:22 -0800

    12141384 Penjelasan public static void main (String[]args) public static void main(String[] args) adalah fungsi main dimana public pada bagian ini berarti bahwa metode-metode dapat dipanggil didalam dan juga diluar kelas.

---
+++ src/class12141384.java
@@ -0,0 +1,9 @@
+import com.sigit.app12141384.TebakAngka;
+
+public class class12141384 {
+    public static void main(String[] args) {
+    TebakAngka game = new TebakAngka();
+        game.Games();
+}
+    
+}
