99f682720010b5b06bc3e24e6d68e7aea3da1d8b
Mr.She-Gate <Mr.She-Gate@She-Gate>
2016-11-22 12:36:17 -0800

    1214134 Public digunakan untuk menyatakan bahwa kelas/method/attribute tersebut dapat diakses oleh kelas lain dimanapun.

--- src/com/sigit/app12141384/TebakAngka.java
+++ src/com/sigit/app12141384/TebakAngka.java
@@ -8,3 +8,13 @@ import java.util.Scanner;
  * NIM 12141384
  * @author Mr.She-Gate
  */
+public class TebakAngka {
+
+    public String namaPemakai;
+    public int asalTebakAngka;
+    public byte tingkatan;
+    public char jawab;
+    public int angkaTebakAcak;
+    public int cobaLagi;
+    public int nilai;
+    
